9dcca24c02a3bd722827922de856141ab8aa9dd7
zeal4u <jsz1995@live.com>
2015-09-17 21:17:06 +0800

    这些都是po

---
+++ src/main/java/domain/Talent.java
@@ -0,0 +1,43 @@
+package domain;
+
+/**
+ * Created by Jsz on 2015/9/17.
+ */
+public class Talent {
+    private String id;
+    private String empno;
+    private String jobid;
+    private String time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(String empno) {
+        this.empno = empno;
+    }
+
+    public String getJobid() {
+        return jobid;
+    }
+
+    public void setJobid(String jobid) {
+        this.jobid = jobid;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
