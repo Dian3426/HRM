90f558ade5d53ee1c5c45783265e8b55fa1fbe13
Dian3426 <18007320392@163.com>
2015-09-17 14:53:07 +0800

    空项目测试

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ML3426 on 2015/9/17 0017.
+ *
+ * @author ML3426
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("项目测试");
+    }
+}
