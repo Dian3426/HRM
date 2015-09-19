16e181faad879d8e32cd1fa85c296543f08c4df5
zeal4u <jsz1995@live.com>
2015-09-19 17:59:49 +0800

    部门表的映射类

--- src/main/java/mapper/SkdeptManager.java
+++ src/main/java/mapper/SkdeptManager.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -12,14 +13,16 @@ import java.util.List;
  */
 public interface SkdeptManager {
 
-    @Insert("INSERT INTO SKDEPT VALUES (NULL ,#{Skdept.dept_id},#{Skdept.name},#{Skdept.type},#{Skdept.tele}," +
+    @Insert("INSERT INTO SKDEPT VALUES (NULL ,#{Skdept.dept_id},#{Skdept.name},#{Skdept.type,typeHandler=org.apache.ibatis.type.EnumTypeHandler},#{Skdept.tele}," +
             "#{Skdept.fax},#{Skdept.discrip,jdbcType=VARCHAR},#{Skdept.superd,jdbcType=VARCHAR} ,#{Skdept.createtime})")
     void createDept(@Param("Skdept") Skdept skdept);
 
     @Select("SELECT *  FROM SKDEPT")
     List<Skdept> getAllDepts();
 
-    @Select(" select * from SKDEPT rownum<=#{end}  minus  select * from SKDEPT rownum>=#{start}")
-    List<Skdept> getDeptsByRows(int start,int end );
+    @Select(" select * from SKDEPT WHERE rownum<=#{end}  minus  select * from SKDEPT WHERE rownum>=#{start}")
+    List<Skdept> getDeptsByRows(@Param("start")int start,@Param("end")int end );
 
+    @Select("SELECT DEPT_ID, NAME FROM SKDEPT")
+    HashMap<String,String> getAllDeptidAndNames();
 }
